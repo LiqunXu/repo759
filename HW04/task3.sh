02c9c5d79339dba1136e4872d084a1e64a55ef33
LIQUN XU <lxu322@wisc.edu>
2024-10-30 18:43:20 -0500

    task3.sh

---
+++ HW04/task3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#SBATCH -p instruction
+#SBATCH -J task3
+#SBATCH -o task3.out -e task3.err
+#SBATCH --cpus-per-task=8
+
+if [ "$1" = "compile" ]; then
+  g++ task3.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
+elif [ "$1" = "run" ]; then
+  echo "Running with number of particles, simulation end time, num thread"
+  ./task3 100 100 8
+elif [ "$1" = "clean" ]; then
+  rm -f task3 task3.err task3.out task3.pdf
+else
+  echo "./task1.sh [compile | run | clean]"
+fi
