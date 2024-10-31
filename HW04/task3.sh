2fb6e1762a444840aef710b2529c21ea636dad98
LIQUN XU <lxu322@wisc.edu>
2024-10-30 19:27:14 -0500

    task3.sh

--- HW04/task3.sh
+++ HW04/task3.sh
@@ -8,8 +8,8 @@
 if [ "$1" = "compile" ]; then
   g++ task3.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 elif [ "$1" = "run" ]; then
-  echo "Running with number of particles, simulation end time, num thread"
-  ./task3 100 100 8
+  echo "Running with number of particles, simulation end time, num thread" >> task3.out
+  ./task3 100 100 8 >> task3.out 2>&1
 elif [ "$1" = "clean" ]; then
   rm -f task3 task3.err task3.out task3.pdf
 else
