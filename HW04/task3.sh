199f5783c462b419b43ae6d618e7b8246d184ef9
LIQUN XU <lxu322@wisc.edu>
2024-10-30 19:49:04 -0500

    task3.sh

--- HW04/task3.sh
+++ HW04/task3.sh
@@ -6,16 +6,14 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --time=01:00:00
 
-if [ "$1" = "compile" ]; then
-  g++ task3.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
-elif [ "$1" = "run" ]; then
-  particles=100
-  end_time=100.0
-  executable=task3
-  for schedule in static dynamic guided; do
-      for threads in {1..8}; do
-          echo "Running with $threads threads and $schedule scheduling"
-          ./$executable $particles $end_time $threads $schedule
-      done
-  done
-fi
+particles=100
+end_time=100.0
+executable=task3
+
+for schedule in static dynamic guided; do
+    for threads in {1..8}; do
+        echo "Running with $threads threads and $schedule scheduling"
+        ./$executable $particles $end_time $threads $schedule
+    done
+done
+
