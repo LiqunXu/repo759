cfa92357759c1db96c7cbb77e962bb413e9dd53d
LIQUN XU <lxu322@wisc.edu>
2024-11-20 19:06:51 -0600

    task1

--- HW06/task1_copy.sh
+++ HW06/task1_copy.sh
@@ -9,7 +9,7 @@
 #SBATCH --gres=gpu:1
 module purge
 module load nvidia/cuda/11.8.0
-module load gcc/11.3.0
+module load gcc/13.2.0
 
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
 
