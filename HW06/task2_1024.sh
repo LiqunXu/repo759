ca01520a10384d8da36895afd10c04a108dec105
LIQUN XU <lxu322@wisc.edu>
2024-11-20 20:25:09 -0600

    task2

--- HW06/task2_1024.sh
+++ HW06/task2_1024.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=task1
+#SBATCH --job-name=task2
 #SBATCH -p instruction
 #SBATCH --ntasks=1 --cpus-per-task=2
 #SBATCH --time=0-00:10:00
-#SBATCH --output="task1.out"
-#SBATCH --error="task1.err"
+#SBATCH --output="task2.out"
+#SBATCH --error="task2.err"
 
 #SBATCH --gres=gpu:1
 module purge
@@ -18,7 +18,8 @@ THREADS_BLOCK1=1024
 R=128
 
 # Run for each value of n and both thread configurations
-for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216; do
+for ((i=10; i<=29; i++)); do
+    n=$((2**i))
     for THREADS in $THREADS_BLOCK1; do
         ./task2 $n $R $THREADS
 
