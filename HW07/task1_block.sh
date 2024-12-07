8cb601a6a8e196dac4412895de6c6a33b9600a42
LIQUN XU <lxu322@wisc.edu>
2024-12-06 21:04:31 -0600

    task1

--- HW07/task1_block.sh
+++ HW07/task1_block.sh
@@ -3,10 +3,12 @@
 #SBATCH -p instruction
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=0-00:10:00
+#SBATCH --time=0-00:20:00
 #SBATCH --output="task12.out"
 #SBATCH --error="task12.err"
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
+#SBATCH --mem=32G
+
 
 module purge
 module load nvidia/cuda/11.8.0
@@ -15,7 +17,7 @@ module load nvidia/cuda/11.8.0
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
 
 # Parameters
-n=384
+n=16384
 
 # Avoid invalid characters in loops and optimize resource usage
 for block in {4..12}; do
